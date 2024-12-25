d2b25636341fe3cd4ae2b4103e3a6b08c119652b
chison2707 <chiphansonzz17@gmail.com>
2024-12-25 09:26:23 +0700

    lấy ra danh sách bài viết

--- resolvers.ts
+++ resolvers.ts
@@ -1,7 +1,15 @@
+import Article from "./models/article.model";
+
 export const resolvers = {
     Query: {
         hello: () => {
             return "Hello world!"
+        },
+        getListArticle: async () => {
+            const article = await Article.find({
+                deleted: false
+            });
+            return article;
         }
     }
 };
